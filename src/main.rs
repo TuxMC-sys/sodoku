9c014234ca220158cd556c2e280a21594fad08ef
TuxMC-sys <109832568+TuxMC-sys@users.noreply.github.com>
2024-04-09 11:04:46 -0400

    current progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use ndarray::prelude::*;
-use ndarray::Array;
-const EXAMPLE_PUZZLE: [[u8;9];9] = [[0,0,0,0,0,0,0,0,0],
+use ndarray::{array,Array, Ix2, Axis, ArrayView};
+use std::collections::HashSet;
+
+fn main() {
+    let a = array![[0,0,0,0,0,0,0,0,0],
     [1,2,3,4,5,6,0,0,0],
     [0,0,0,0,0,0,7,8,9],
     [0,0,0,0,0,4,0,0,0],
@@ -9,8 +11,28 @@ const EXAMPLE_PUZZLE: [[u8;9];9] = [[0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0]];
-fn main() {
-    let test = Array::<u8, Ix2>::zeros((9,9));
-    //let column = new_puzzle(EXAMPLE_PUZZLE);
-    println!("{:?}",test);
+  
+    //println!("{}", puzzle_valid(&a));
+}
+fn row_valid(arr: &Array<i32, Ix2>, check_row: usize) -> bool{
+    let row_set: HashSet<i32> = arr.row(check_row).to_vec().into_iter().collect();
+    match row_set.len(){
+        9 => true,
+        _ => false
+    }
+}
+fn column_valid(arr: &Array<i32, Ix2>, check_col: usize) -> bool{
+    let col_set: HashSet<i32> = arr.column(check_col).to_vec().into_iter().collect();
+    match col_set.len(){
+        9 => true,
+        _ => false
+    }
+}
+/*fn square_valid(arr: &ArrayView<'a, &i32, Ix2>, check_sq: u8) -> bool{
+    let check_h = (check_sq - 1) - ((check_sq - 1) % 3);
+    let (split_above, split_below) = arr.split_at(Axis(0), check_sq-1);
+    true
+}*/
+fn get_square<'a>(arr: &ArrayView<'a, i32, Ix2>, sq: usize) -> HashSet<i32>{
+
 }
